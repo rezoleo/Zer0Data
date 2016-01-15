7638a9850fede8bbde3f5653d152f907a3650254
Thomas Gaudin <thomas.gaudin@centraliens-lille.org>
2016-01-15 12:17:49 +0100

    Switched to HTTP API
    Signed-off-by: Thomas Gaudin <thomas.gaudin@centraliens-lille.org>

--- app/src/main/java/com/rezoleo/zer0data/common/Common.java
+++ app/src/main/java/com/rezoleo/zer0data/common/Common.java
@@ -4,5 +4,5 @@ package com.rezoleo.zer0data.common;
  * Created by Thomas on 8/01/16.
  */
 public class Common extends fr.applicationcore.Common {
-    public static String URL = "https://localhost:9301";
+    public static String URL = "http://localhost:9301";
 }
