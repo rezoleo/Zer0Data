2f76a0095958127c427d1db23a4cf7a772863430
Linkorange <judorange1992@yahoo.fr>
2016-01-16 21:55:00 +0100

    Deleted test_activity

--- app/src/main/java/com/rezoleo/zer0data/InformationsActivity.java
+++ app/src/main/java/com/rezoleo/zer0data/InformationsActivity.java
@@ -2,6 +2,9 @@ package com.rezoleo.zer0data;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class InformationsActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class InformationsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_informations);
+
     }
+
+    protected void hideLinearLayout(RelativeLayout layoutToHide)
+    {
+        layoutToHide.setVisibility(View.GONE);
+    }
+
 }
