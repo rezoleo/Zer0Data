5443642158054771a1f4665df19f5aace1a2b7cd
Thomas Gaudin <thomas.gaudin@centraliens-lille.org>
2016-01-17 13:43:49 +0100

    Implemented login logic
    Signed-off-by: Thomas Gaudin <thomas.gaudin@centraliens-lille.org>

--- app/src/main/java/com/rezoleo/zer0data/MainActivity.java
+++ app/src/main/java/com/rezoleo/zer0data/MainActivity.java
@@ -2,6 +2,10 @@ package com.rezoleo.zer0data;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.rezoleo.zer0data.network.AsyncLoginClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
+    }
+
+    public void connect(View v) {
+        TextView tvLogin = (TextView) findViewById(R.id.login);
+        TextView tvPassword = (TextView) findViewById(R.id.password);
+        String login = null;
+        try {
+            login = tvLogin.getText().toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        String password = null;
+        try {
+            password = tvPassword.getText().toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        new AsyncLoginClient(this).execute(login, password);
     }
 }
