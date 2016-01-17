a861ee3054e322a7c4075f7d078b6df4a7eeda93
Thomas Gaudin <thomas.gaudin@centraliens-lille.org>
2016-01-17 14:35:50 +0100

    Made the LoginInformation parcelable, to pass it between Activities
    Signed-off-by: Thomas Gaudin <thomas.gaudin@centraliens-lille.org>

--- app/src/main/java/com/rezoleo/zer0data/object/LoginInformation.java
+++ app/src/main/java/com/rezoleo/zer0data/object/LoginInformation.java
@@ -1,15 +1,41 @@
 package com.rezoleo.zer0data.object;
 
-import java.util.SortedSet;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.List;
 
 /**
  * Created by Thomas on 8/01/16.
  */
-public class LoginInformation extends fr.applicationcore.object.APIObject {
+public class LoginInformation extends fr.applicationcore.object.APIObject implements Parcelable {
     protected String login;
     protected String gate;
-    protected SortedSet<String> roles;
+    protected List<String> roles;
 
+
+    /**
+     * Static field used to regenerate object, individually or as arrays
+     */
+    public static final Parcelable.Creator<LoginInformation> CREATOR = new Parcelable.Creator<LoginInformation>() {
+        @Override
+        public LoginInformation createFromParcel(Parcel source) {
+            return new LoginInformation(source);
+        }
+        @Override
+        public LoginInformation[] newArray(int size) {
+            return new LoginInformation[size];
+        }
+    };
+
+    /**
+     * Creator from Parcel, reads back fields IN THE ORDER they were written
+     */
+    public LoginInformation(Parcel pc) {
+        login = pc.readString();
+        gate = pc.readString();
+        pc.readStringList(roles);
+    }
 
     public String getLogin() {
         return login;
@@ -18,5 +44,17 @@ public class LoginInformation extends fr.applicationcore.object.APIObject {
     @Override
     public boolean isEmpty() {
         return login == null;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(login);
+        dest.writeString(gate);
+        dest.writeStringList(roles);
     }
 }
