f18531f6ddc8577fe40c184cbc8c854d9daa958e
Thomas Gaudin <thomas.gaudin@centraliens-lille.org>
2016-03-06 18:09:59 +0100

    Added service_card.jar dependency. Now all object classes extend service_* objects.
    Signed-off-by: Thomas Gaudin <thomas.gaudin@centraliens-lille.org>

--- app/src/main/java/com/rezoleo/zer0data/object/Card.java
+++ app/src/main/java/com/rezoleo/zer0data/object/Card.java
@@ -1,28 +1,20 @@
 package com.rezoleo.zer0data.object;
 
+import java.util.Date;
+
 /**
  * Created by Thomas on 8/01/16.
  */
-public class Card implements fr.applicationcore.object.APIObject {
+public class Card extends fr.service_card.object.Card implements fr.applicationcore.object.APIObject {
     protected boolean registered;
-    protected String code;
-    protected String owner;
-    protected String status;
 
-    public String getCode() {
-        return code;
-    }
-
-    public String getOwner() {
-        return owner;
-    }
-
-    public String getStatus() {
-        return status;
+    public Card(String _id, String owner, String code, String status, String creator, Date created, String creatorService, String updator, Date updated, String updatorService, boolean registered) {
+        super(_id, owner, code, status, creator, created, creatorService, updator, updated, updatorService);
+        this.registered = registered;
     }
 
     @Override
     public boolean isEmpty() {
-        return !registered;
+        return !this.registered;
     }
 }
--- app/src/main/java/com/rezoleo/zer0data/object/Contributor.java
+++ app/src/main/java/com/rezoleo/zer0data/object/Contributor.java
@@ -1,8 +1,5 @@
 package com.rezoleo.zer0data.object;
 
-import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.Since;
-
 import java.util.Date;
 
 import fr.applicationcore.object.APIObject;
@@ -10,140 +7,25 @@ import fr.applicationcore.object.APIObject;
 /**
  * Created by Thomas on 8/01/16.
  */
-public class Contributor implements APIObject {
+public class Contributor extends fr.service_contributor.object.Contributor implements APIObject {
     protected boolean registered;
-    @Expose
-    @Since(1.0D)
-    protected String _id = null;
-    @Expose
-    @Since(1.0D)
-    protected String login = null;
-    @Expose
-    @Since(1.0D)
-    protected String creator = null;
-    @Expose
-    @Since(1.0D)
-    protected Date created = null;
-    @Expose
-    @Since(1.0D)
-    protected String creatorService = null;
 
-    public Contributor() {
+    public Contributor(String _id, String login, String creator, Date created, String creatorService, boolean registered) {
+        super(_id, login, creator, created, creatorService);
+        this.registered = registered;
     }
 
-    public Contributor(String _id, String login, String creator, Date created, String creatorService) {
-        this._id = _id;
-        this.login = login;
-        this.creator = creator;
-        this.created = created;
-        this.creatorService = creatorService;
-    }
-
-    public String get_id() {
-        return this._id;
-    }
-
-    public void set_id(String _id) {
-        this._id = _id;
-    }
-
-    public String getLogin() {
-        return this.login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getCreator() {
-        return this.creator;
-    }
-
-    public void setCreator(String creator) {
-        this.creator = creator;
-    }
-
-    public Date getCreated() {
-        return this.created;
-    }
-
-    public void setCreated(Date created) {
-        this.created = created;
-    }
-
-    public String getCreatorService() {
-        return this.creatorService;
-    }
-
-    public void setCreatorService(String creatorService) {
-        this.creatorService = creatorService;
-    }
-
+    @Override
     public int hashCode() {
-        boolean prime = true;
-        byte result = 1;
-        int result1 = 31 * result + (this._id == null?0:this._id.hashCode());
-        result1 = 31 * result1 + (this.created == null?0:this.created.hashCode());
-        result1 = 31 * result1 + (this.creator == null?0:this.creator.hashCode());
-        result1 = 31 * result1 + (this.creatorService == null?0:this.creatorService.hashCode());
-        result1 = 31 * result1 + (this.login == null?0:this.login.hashCode());
-        return result1;
+        return super.hashCode();
     }
 
+    @Override
     public boolean equals(Object obj) {
-        if(this == obj) {
-            return true;
-        } else if(obj == null) {
-            return false;
-        } else if(this.getClass() != obj.getClass()) {
-            return false;
-        } else {
-            Contributor other = (Contributor)obj;
-            if(this._id == null) {
-                if(other._id != null) {
-                    return false;
-                }
-            } else if(!this._id.equals(other._id)) {
-                return false;
-            }
-
-            if(this.created == null) {
-                if(other.created != null) {
-                    return false;
-                }
-            } else if(!this.created.equals(other.created)) {
-                return false;
-            }
-
-            if(this.creator == null) {
-                if(other.creator != null) {
-                    return false;
-                }
-            } else if(!this.creator.equals(other.creator)) {
-                return false;
-            }
-
-            if(this.creatorService == null) {
-                if(other.creatorService != null) {
-                    return false;
-                }
-            } else if(!this.creatorService.equals(other.creatorService)) {
-                return false;
-            }
-
-            if(this.login == null) {
-                if(other.login != null) {
-                    return false;
-                }
-            } else if(!this.login.equals(other.login)) {
-                return false;
-            }
-
-            return true;
-        }
+        return super.equals(obj);
     }
 
     public boolean isEmpty() {
-            return !registered;
+            return !this.registered;
         }
 }
