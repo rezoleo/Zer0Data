2cee34ab60ae60a65adbf16b13e907b2e881bf98
Thomas Gaudin <thomas.gaudin@centraliens-lille.org>
2016-04-18 22:59:36 +0200

    Added documentation
    Signed-off-by: Thomas Gaudin <thomas.gaudin@centraliens-lille.org>

--- app/src/main/java/com/rezoleo/zer0data/object/GlobalState.java
+++ app/src/main/java/com/rezoleo/zer0data/object/GlobalState.java
@@ -8,6 +8,13 @@ import android.app.Application;
 public class GlobalState extends Application {
     protected static SesssionStatus sesssionStatus = SesssionStatus.disconnected;
 
+    /**
+     * This function is used to store and change the connection status, while guaranteeing a single
+     * connection request at a time (with the synchronized keyword).
+     * @param old_Sesssion_status The status that should be before this moment
+     * @param new_Sesssion_status The new status for the session
+     * @return true if the status was changed, false if the status wasn't changed
+     */
     public static synchronized boolean checkAndReplace(SesssionStatus old_Sesssion_status, SesssionStatus new_Sesssion_status){
         if(old_Sesssion_status !=null && GlobalState.sesssionStatus != old_Sesssion_status){
             return false;
